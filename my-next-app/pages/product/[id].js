23c0f8551e123cd2ee792c88fbd6414ab49d74ed
vinh nguyen <88311053+vinhnq-devblock@users.noreply.github.com>
2023-05-17 14:32:01 +0700

    init

---
+++ my-next-app/pages/product/[id].js
@@ -0,0 +1,13 @@
+import React from "react";
+import { useRouter } from "next/router";
+export default function ProductDetail() {
+  const router = useRouter();
+  return (
+    <div style={{ marginLeft: "20px", marginTop: "15px" }}>
+      <span
+        style={{ color: "red", fontSize: "22px", fontWeight: "bold" }}
+      >{JSON.stringify(router.query, null, 0)}</span>
+      
+    </div>
+  );
+}
