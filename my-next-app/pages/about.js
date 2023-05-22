c2434951d94c2dc2b2e9e38ad101418b07eb4799
vinh nguyen <88311053+vinhnq-devblock@users.noreply.github.com>
2023-05-22 11:17:51 +0700

    commit

--- my-next-app/pages/about.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function about() {
-  return (
-    <div>About page</div>
-  )
-}
---
+++ my-next-app/pages/post/[id].js
@@ -0,0 +1,32 @@
+export default function Post({ post }) {
+  return <div>{post.body}</div>;
+}
+
+// This function gets called at build time
+export async function getStaticPaths() {
+  // Call an external API endpoint to get posts
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const posts = await res.json();
+
+  // Get the paths we want to pre-render based on posts
+  const paths = posts.map((post) => ({
+    params: { id: post.id.toString() },
+  }));
+
+  // We'll pre-render only these paths at build time.
+  // { fallback: false } means other routes should 404.
+  return { paths, fallback: false };
+}
+
+export async function getStaticProps({ params }) {
+  console.log(params,'params')
+  // params contains the post `id`.
+  // If the route is like /posts/1, then params.id is 1
+  const res = await fetch(
+    `https://jsonplaceholder.typicode.com/posts/${params.id}`
+  );
+  const post = await res.json();
+
+  // Pass post data to the page via props
+  return { props: { post } };
+}
